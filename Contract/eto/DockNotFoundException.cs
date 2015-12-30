16eb7ab8571798ecace75e8e3dd4f2690f028e73
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-12-30 21:21:57 +0100

    DockNotFoundException made by the backend, as the frontenders did not commit the file it self

---
+++ Contract/eto/DockNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.eto
+{
+    /// <summary>
+    /// Made by the backend, as the frontenders did not commit the file it self, or even check there repository....
+    /// </summary>
+    public class DockNotFoundException : Exception
+    {
+    }
+}
