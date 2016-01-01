ca5a3caa02d13db9c4af7feb16316b11d7fbbff6
Thomas D. Kryger <tdkryger@akvariet.dk>
2016-01-01 12:18:14 +0100

    a

---
+++ UnitTest/AdministrationBETests.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FerryBackendB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerryBackendB.Tests
+{
+    [TestClass()]
+    public class AdministrationBETests
+    {
+        [TestMethod()]
+        public void CreateCustomerTest()
+        {
+            AdministrationBE a = new AdministrationBE();
+            Contract.dto.Customer c = UnitTest.TestHelpers.randomCustomer();
+            c = a.CreateCustomer(c);
+            Assert.AreNotEqual(0, c.CustomerId);
+        }
+
+
+        [TestMethod()]
+        public void CreateDockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateFerryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateReservationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateRouteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTripTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateVehicleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteCustomerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteDockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteFerryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteReservationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteRouteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTripTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteVehicleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllDocksTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllFerriesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllReservationsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllRoutesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTripsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllVehiclesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFerryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetReservationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetRouteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTripTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetVehicleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateCustomerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateDockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateFerryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateReservationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateRouteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTripTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateVehicleTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ UnitTest/TestHelpers.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    static class TestHelpers
+    {
+        #region helpers
+        private static Random rnd = new Random(DateTime.Now.Millisecond);
+        /// <summary>
+        /// Generates a random id
+        /// </summary>
+        /// <returns> int between 1 and int.MaxValue</returns>
+        public static int GenerateRandomId()
+        {
+            return rnd.Next(1, int.MaxValue);
+        }
+
+        public static int GenerateRandomId(int min, int max)
+        {
+            return rnd.Next(min, max);
+        }
+
+        public static string RandomWords(int num_letters)
+        {
+            // Make an array of the letters we will use.
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            // Make a random number generator.
+            Random rand = new Random();
+
+            // Make the words.
+            // Make a word.
+            string word = string.Empty;
+            for (int j = 1; j <= num_letters; j++)
+            {
+                // Pick a random number between 0 and 25
+                // to select a letter from the letters array.
+                int letter_num = rand.Next(0, letters.Length - 1);
+
+                // Append the letter.
+                word += letters[letter_num];
+            }
+            return word;
+        }
+
+        public static Contract.dto.Customer randomCustomer()
+        {
+            int free = GenerateRandomId(0, 10);
+            return new Contract.dto.Customer()
+            {
+                AmountOfFreeRides = free,
+                City = RandomWords(10),
+                CustomerId = 0,
+                Firstname = RandomWords(10),
+                HouseNumber = GenerateRandomId(1, 300).ToString(),
+                Lastname = RandomWords(10),
+                Mail = RandomWords(10),
+                Native = (free > 0),
+                Password = RandomWords(10),
+                Phone = "12345678",
+                PostalCode = 1,
+                Street = RandomWords(10)
+            };
+        }
+
+        public static Contract.dto.Dock randomDock()
+        {
+            return new Contract.dto.Dock()
+            {
+                DockId = GenerateRandomId(),
+                DockName = RandomWords(25),
+                FerryCapacity = GenerateRandomId(1, 10)
+            };
+        }
+        #endregion
+    }
+}
