bf60f26f5be2fc931c6369a95fc7ba71cbac77d8
Thomas D. Kryger <tdkryger@akvariet.dk>
2016-01-01 12:58:03 +0100

    Muckups done

--- FerryBackendB/RouteHandler.cs
+++ FerryBackendB/RouteHandler.cs
@@ -7,32 +7,49 @@ namespace FerryBackendB
     {
         public static Contract.dto.Route random()
         {
-            throw new NotImplementedException();
+            return new Contract.dto.Route()
+            {
+                Depature = MySQLConn.RandomWords(30),
+                Destination = MySQLConn.RandomWords(30),
+                Duration = MySQLConn.GenerateRandomId(10, 1000),
+                RouteId = MySQLConn.GenerateRandomId()
+            };
         }
 
         public static Contract.dto.Route CreateRoute(Contract.dto.Route route)
         {
-            throw new NotImplementedException();
+            route.RouteId = MySQLConn.GenerateRandomId();
+            return route;
         }
 
         public static Contract.dto.Route GetRoute(int routeId)
         {
-            throw new NotImplementedException();
+            Contract.dto.Route r = random();
+            r.RouteId = routeId;
+            return r;
         }
 
         public static List<Contract.dto.Route> GetAllRoutes() // Stupids
         {
-            throw new NotImplementedException();
+            List<Contract.dto.Route> l = new List<Contract.dto.Route>();
+            int max = MySQLConn.GenerateRandomId(10, 10000);
+            for(int i=1; i<=max;i++)
+            {
+                Contract.dto.Route r = random();
+                r.RouteId = i;
+                l.Add(r);
+            }
+            return l;
         }
 
         public static Contract.dto.Route UpdateRoute(Contract.dto.Route route)
         {
-            throw new NotImplementedException();
+            return route;
         }
 
         public static bool DeleteRoute(Contract.dto.Route route)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
