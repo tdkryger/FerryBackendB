c07ac243f1725139b85b307f837fb19b567326ae
Thomas D. Kryger <tdkryger@akvariet.dk>
2016-01-01 12:15:11 +0100

    Muckup for dockhandler

--- FerryBackendB/DockHandler.cs
+++ FerryBackendB/DockHandler.cs
@@ -5,30 +5,53 @@ namespace FerryBackendB
 {
     static class DockHandler
     {
+        #region Private methods
+        private static Contract.dto.Dock randomDock()
+        {
+            return new Contract.dto.Dock()
+            {
+                DockId = MySQLConn.GenerateRandomId(),
+                DockName = MySQLConn.RandomWords(25),
+                FerryCapacity = MySQLConn.GenerateRandomId(1, 10)
+            };
+        }
+        #endregion
+
         public static Contract.dto.Dock CreateDock(Contract.dto.Dock dock)
         {
-            throw new NotImplementedException();
+            dock.DockId = MySQLConn.GenerateRandomId();
+            return dock;
         }
 
         public static List<Contract.dto.Dock> GetAllDocks() // silly ppl
         {
-            throw new NotImplementedException();
+            List<Contract.dto.Dock> l = new List<Contract.dto.Dock>();
+            int max = MySQLConn.GenerateRandomId(2, 10000);
+            for (int i = 1; i < max; i++)
+            {
+                Contract.dto.Dock d = randomDock();
+                d.DockId = i;
+                l.Add(d);
+            }
+            return l;
         }
 
         public static Contract.dto.Dock GetDock(int dockId)
         {
-            throw new NotImplementedException();
+            Contract.dto.Dock d = randomDock();
+            d.DockId = dockId;
+            return d;
         }
 
         public static Contract.dto.Dock UpdateDock(Contract.dto.Dock dock)
         {
-            throw new NotImplementedException();
+            return dock;
         }
 
 
         public static bool DeleteDock(Contract.dto.Dock dock)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
