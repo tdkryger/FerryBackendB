6468177ce09063cbaef82fa9498f87fb222fa221
Thomas D. Kryger <tdkryger@akvariet.dk>
2016-01-01 13:05:17 +0100

    Muckups done

--- FerryBackendB/VehicleHandler.cs
+++ FerryBackendB/VehicleHandler.cs
@@ -7,32 +7,49 @@ namespace FerryBackendB
     {
         public static Contract.dto.Vehicle random()
         {
-            throw new NotImplementedException();
+            return new Contract.dto.Vehicle()
+            {
+                VehicleId = MySQLConn.GenerateRandomId(),
+                VehiclePrice = (double)(MySQLConn.GenerateRandomId(1000, 10000) / 10),
+                VehicleSize = MySQLConn.GenerateRandomId(1, 10),
+                VehicleType = MySQLConn.RandomWords(10)
+            };
         }
 
         public static Contract.dto.Vehicle CreateVehicle(Contract.dto.Vehicle vehicle)
         {
-            throw new NotImplementedException();
+            vehicle.VehicleId = MySQLConn.GenerateRandomId();
+            return vehicle;
         }
 
         public static Contract.dto.Vehicle GetVehicle(int vehicleId)
         {
-            throw new NotImplementedException();
+            Contract.dto.Vehicle v = random();
+            v.VehicleId = vehicleId;
+            return v;
         }
 
         public static List<Contract.dto.Vehicle> GetAllVehicles() // Fools
         {
-            throw new NotImplementedException();
+            List<Contract.dto.Vehicle> l = new List<Contract.dto.Vehicle>();
+            int max = MySQLConn.GenerateRandomId(10000, 1000000);
+            for (int i = 1; i <= max; i++)
+            {
+                Contract.dto.Vehicle r = random();
+                r.VehicleId = i;
+                l.Add(r);
+            }
+            return l;
         }
 
         public static Contract.dto.Vehicle UpdateVehicle(Contract.dto.Vehicle vehicle)
         {
-            throw new NotImplementedException();
+            return vehicle;
         }
 
         public static bool DeleteVehicle(Contract.dto.Vehicle vehicle)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
