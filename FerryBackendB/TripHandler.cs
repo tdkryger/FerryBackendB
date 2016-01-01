7cf6bf872c63f2cc8e134594dcaa2e238341d265
Thomas D. Kryger <tdkryger@akvariet.dk>
2016-01-01 13:01:53 +0100

    Muckups done

--- FerryBackendB/TripHandler.cs
+++ FerryBackendB/TripHandler.cs
@@ -7,33 +7,51 @@ namespace FerryBackendB
     {
         public static Contract.dto.Trip random()
         {
-            throw new NotImplementedException();
+            return new Contract.dto.Trip()
+            {
+                DepatureTime = DateTime.Now,
+                Ferry = FerryHandler.random(),
+                Route = RouteHandler.random(),
+                TripId = MySQLConn.GenerateRandomId(),
+                TripPrice = (double)(MySQLConn.GenerateRandomId(1000, 10000) / 10)
+            };
         }
 
         public static Contract.dto.Trip CreateTrip(Contract.dto.Trip trip)
         {
-            throw new NotImplementedException();
+            trip.TripId = MySQLConn.GenerateRandomId();
+            return trip;
         }
 
         public static Contract.dto.Trip GetTrip(int tripId)
         {
-            throw new NotImplementedException();
+            Contract.dto.Trip trip = random();
+            trip.TripId = MySQLConn.GenerateRandomId();
+            return trip;
         }
 
         public static List<Contract.dto.Trip> GetAllTrips()
         {
-            throw new NotImplementedException();
+            List<Contract.dto.Trip> l = new List<Contract.dto.Trip>();
+            int max = MySQLConn.GenerateRandomId(10, 10000);
+            for (int i = 1; i <= max; i++)
+            {
+                Contract.dto.Trip r = random();
+                r.TripId = i;
+                l.Add(r);
+            }
+            return l;
         }
 
         public static Contract.dto.Trip UpdateTrip(Contract.dto.Trip trip)
         {
-            throw new NotImplementedException();
+            return trip;
         }
 
 
         public static bool DeleteTrip(Contract.dto.Trip trip)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
