5c096dbdccc4ec3f9df43a33d871eb866c3dc54b
Thomas D. Kryger <tdkryger@akvariet.dk>
2016-01-01 11:54:20 +0100

    muck methods

--- FerryBackendB/CustomerHandler.cs
+++ FerryBackendB/CustomerHandler.cs
@@ -7,31 +7,95 @@ namespace FerryBackendB
     {
         public static Contract.dto.Customer CreateCustomer(Contract.dto.Customer customer)
         {
-            throw new NotImplementedException();
+            customer.CustomerId = MySQLConn.GenerateRandomId();
+            return customer;
+
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username">dunno where to put this.. Sticking it in email..</param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public static Contract.dto.Customer ReadCustomer(string username, string password)
         {
-            throw new NotImplementedException();
+            int free = MySQLConn.GenerateRandomId(0, 10);
+            return new Contract.dto.Customer()
+            {
+                AmountOfFreeRides = free,
+                City = "Andeby",
+                CustomerId = MySQLConn.GenerateRandomId(),
+                Firstname = "Anders",
+                HouseNumber = "111",
+                Lastname = "And",
+                Mail = username,
+                Native = (free > 0),
+                Password = password,
+                Phone = "12345678",
+                PostalCode = 1,
+                Street = "Paradisæblevej"
+            };
         }
         public static Contract.dto.Customer ReadCustomer(string mail)
         {
-            throw new NotImplementedException();
+            int free = MySQLConn.GenerateRandomId(0, 10);
+            return new Contract.dto.Customer()
+            {
+                AmountOfFreeRides = free,
+                City = "Andeby",
+                CustomerId = MySQLConn.GenerateRandomId(),
+                Firstname = "Anders",
+                HouseNumber = "111",
+                Lastname = "And",
+                Mail = mail,
+                Native = (free > 0),
+                Password = "",
+                Phone = "12345678",
+                PostalCode = 1,
+                Street = "Paradisæblevej"
+            };
         }
-
+        /// <summary>
+        /// Retuens a list of somewhat random customers
+        /// </summary>
+        /// <returns></returns>
         public static List<Contract.dto.Customer> ReadAllCustomer()
         {
-            throw new NotImplementedException();
+            List<Contract.dto.Customer> ll = new List<Contract.dto.Customer>();
+            int max = MySQLConn.GenerateRandomId(10, 10000);
+            for (int i = 1; i < max; i++)
+            {
+                int free = MySQLConn.GenerateRandomId(0, 10);
+                Contract.dto.Customer c = new Contract.dto.Customer()
+                {
+                    AmountOfFreeRides = free,
+                    City = MySQLConn.RandomWords(10),
+                    CustomerId = i,
+                    Firstname = MySQLConn.RandomWords(10),
+                    HouseNumber = MySQLConn.GenerateRandomId(1, 300).ToString(),
+                    Lastname = MySQLConn.RandomWords(10),
+                    Mail = MySQLConn.RandomWords(10),
+                    Native = (free > 0),
+                    Password = MySQLConn.RandomWords(10),
+                    Phone = "12345678",
+                    PostalCode = 1,
+                    Street = MySQLConn.RandomWords(10)
+                };
+                ll.Add(c);
+            }
+
+            return ll;
         }
 
         public static Contract.dto.Customer UpdateCustomer(Contract.dto.Customer customer)
         {
-            throw new NotImplementedException();
+            return customer;
         }
 
         public static bool DeleteCustomer(Contract.dto.Customer customer)
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
